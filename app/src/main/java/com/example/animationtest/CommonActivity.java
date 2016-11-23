12e188b08b7c82caa0617bf1318f696ce92210f4
zengjie <zj602497556@qq.com>
2016-11-23 17:46:40 +0800

    学习PorterDuffXfermdoe.

--- app/src/main/java/com/example/animationtest/CommonActivity.java
+++ app/src/main/java/com/example/animationtest/CommonActivity.java
@@ -2,35 +2,15 @@ package com.example.animationtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-import com.example.animationtest.view.RoundImageView;
+import com.example.animationtest.view.XfermodeView;
 
 public class CommonActivity extends AppCompatActivity {
-
-    private RoundImageView mQiqiu;
-    private RoundImageView mMeinv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_common);
-        mMeinv = (RoundImageView) findViewById(R.id.riv_aa);
-        mQiqiu = (RoundImageView) findViewById(R.id.riv_qiqiu);
-
-        mMeinv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mMeinv.setType( 0 );
-            }
-        });
-
-        mQiqiu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mQiqiu.setBorderRadius(70);
-            }
-        });
+        setContentView(new XfermodeView(this));
 
     }
 
---
+++ app/src/main/java/com/example/animationtest/view/XfermodeView.java
@@ -0,0 +1,90 @@
+package com.example.animationtest.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.example.animationtest.R;
+
+/**
+ * Created by ZENGJIE on 2016/11/23.
+ */
+public class XfermodeView extends View {
+
+    private Bitmap mBgBitmap, mFgBitmap;
+    private Paint mPaint;
+    private Path mPath;
+    private Canvas mCanvas;
+
+    public XfermodeView(Context context) {
+        super(context);
+        init();
+    }
+
+    public XfermodeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public XfermodeView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+
+    private void init() {
+        mPaint = new Paint();
+        mPaint.setAlpha(0);
+        mPaint.setXfermode( new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+        mPaint.setStyle(Paint.Style.STROKE);
+        //设置画笔的笔触和连接处更加圆润
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeWidth(50);
+        //设置画笔的笔触和连接处更加圆润
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        mPath = new Path();
+
+        mBgBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.top_image);
+        mFgBitmap = Bitmap.createBitmap(mBgBitmap.getWidth(),
+                mBgBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        //装载画布
+        mCanvas = new Canvas(mFgBitmap);
+        //作用在mFgBitmap上
+        mCanvas.drawColor(Color.GRAY);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(mBgBitmap,0,0,null);
+        canvas.drawBitmap(mFgBitmap,0,0,null);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN :
+                mPath.reset();
+                mPath.moveTo(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_MOVE :
+                mPath.lineTo(event.getX(), event.getY());
+                break;
+        }
+        //drawPath的效果作用在mFgBitmap上
+        mCanvas.drawPath(mPath, mPaint);
+        invalidate();
+        return true;
+    }
+
+}
