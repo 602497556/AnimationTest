4ee10d1720ad06819ac52cd3156dc4232411522e
zengjie <zj602497556@qq.com>
2016-11-28 11:38:27 +0800

    SVG(可伸缩矢量图形)动画.

--- app/src/main/java/com/example/animationtest/CommonActivity.java
+++ app/src/main/java/com/example/animationtest/CommonActivity.java
@@ -1,18 +1,37 @@
 package com.example.animationtest;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.animationtest.view.SimpleDraw;
 import com.example.animationtest.view.XfermodeView;
 
 public class CommonActivity extends AppCompatActivity {
+    
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new SimpleDraw(this));
+        setContentView(R.layout.layout_svg);
+        iv = (ImageView) findViewById(R.id.iv_svg);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animate();
+            }
+        });
+    }
 
+    private void animate() {
+        Drawable drawable = iv.getDrawable();
+        if( drawable instanceof Animatable ){
+            ((Animatable) drawable).start();
+        }
     }
 
 
